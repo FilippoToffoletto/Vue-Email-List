ed3f3b51412a786005cd1e32a1294cc78e35a47d
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-11 17:09:11 +0100

    [add]mail list api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
 const {createApp} = Vue;
 
 createApp({
+
     data(){
         return{
-
+            mailList: [],
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            count: 0,
+            isLoad: false
         }
     },
+
+    methods: {
+        getApi(){
+            this.isLoad = false;
+            this.mailList = [];
+            if(this.count < 15){
+                axios.get(this.apiUrl)
+                .then( result => {
+                    this.mailList.push(result.data.response);
+                })
+                return this.getApi(++this.count)
+            }
+            this.isLoad = true;
+            this.count = 0;
+        },
+    },
+
     mounted(){
-        console.log('montata');
+        //this.getApi();
     }
-}).mount('#app');
+}).mount('#app')
